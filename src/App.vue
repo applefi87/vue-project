77830393fe1a297b1ec7b051c49d3fbe0cee0fbc
applefi87 <werrtyyui@yahoo.com.tw>
2022-06-08 22:11:39 +0800

    做好反向回傳(defineemits)的版本

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <script setup>
-import mainPage from './components/mainPage.vue'
 import cardPage from './components/cardPage.vue'
-// import num from './components/cardPage.vue'
-//
-import { reactive, computed } from 'vue'
-const arr = reactive([
-  { picnum: 1, text: '111', good: false },
-  { picnum: 2, text: '222', good: false },
-  { picnum: 3, text: '333', good: false },
-  { picnum: 4, text: '444', good: false },
-  { picnum: 5, text: '555', good: false }
-])
-//
-const num = computed(() => {
-  return arr.filter(i => { return i.good }).length
-})
+import { ref } from 'vue'
+
+const num = ref(0)
+const numFn = i => {
+  num.value = i
+}
 </script>
 
 <template lang="pug">
-//- mainPage
-hr
 | 按讚總數={{ num }}
 br
-cardPage(v-for="i in arr" :i="i")
-</template>
+cardPage(v-model:numb="num")
 
-<style>
-</style>
+</template>
--- src/components/cardPage.vue
+++ src/components/cardPage.vue
@@ -1,19 +1,31 @@
+<script setup>
+import { reactive, watch } from 'vue'
+const arr = reactive([
+  { picnum: 1, text: '111', good: false },
+  { picnum: 2, text: '222', good: false },
+  { picnum: 3, text: '333', good: false },
+  { picnum: 4, text: '444', good: false },
+  { picnum: 5, text: '555', good: false }
+])
+
+const emit = defineEmits(['numb'])
+
+watch(arr, (n, o) => {
+  const nums = arr.filter(i => { return i.good }).length
+  console.log(nums)
+  emit('update:numb', nums)
+})
+
+</script>
+
 <template lang="pug">
-.card
+.card(v-for="i in arr")
   img(:src="'https://picsum.photos/300/200/?random=' + i.picnum")
   h3 {{ i.text }}
   hr
   button(v-if="i.good" @click="i.good = false") 收回讚
   button(v-else @click="i.good = true") 讚
-
 </template>
-
-<script setup>
-import { reactive, ref } from 'vue'
-const props = defineProps({
-  i: Object
-})
-</script>
 
 <style scoped lang="sass">
 .card
